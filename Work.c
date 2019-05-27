351b463dd85892d9fbfa5e6e941b68ed0b03fd97
ai071655 <AI071655@au.edu.tw>
2019-05-27 16:45:17 +0800

    修改

--- Work.c
+++ Work.c
@@ -1,68 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> //為了寫出Random
+#include <time.h>		//為了寫出Random
 #include <string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-typedef struct { //定義
-	int enter_num[7]; //宣告變數
+typedef struct {		//定義
+	int enter_num[7];	//宣告變數
 }Lotto;
 
 
 int main(int argc, char *argv[])
-{//主函式
- char lotto1[49];//宣告樂透號碼範圍
- //int enter_num[7];
- int n=0,a=0,b=0,i,j;//宣告變數
+{				//主函式
+ 	char lotto1[49];	//宣告樂透號碼範圍
+ 	int n=0,a=0,b=0,i,j;	//宣告變數
 
- Lotto *ptr=NULL;//指標
+ Lotto *ptr=NULL;		//指標
  ptr=(Lotto*)malloc(sizeof(Lotto));
- memset(ptr,0,sizeof(Lotto)); //動態配置記憶體
+ memset(ptr,0,sizeof(Lotto)); 	//動態配置記憶體
  while(scanf("%d",&ptr->enter_num[a]))//迴圈輸入變數
  {
-  a++;//變數增加
-  if(getchar()=='\n'){
-    break;//如果遇到下一行就停止
-  }
+  	a++;			//變數增加
+  	if(getchar()=='\n'){
+    break;			//如果遇到下一行就停止
+ }
  }
 
- srand(time(NULL)); //RANDOM值
+ srand(time(NULL)); 		//RANDOM值
  printf("樂透號碼開獎中.....\n");
 
  for (i=0;i<49;i++)
  {
-  lotto1[i]=i+1;
- }   // 用迴圈使陣列中數字增加
+  	lotto1[i]=i+1;}     	// 用迴圈使陣列中數字增加
  for (i=0;i<=6;i++)
- {   // 為做出樂透號碼1~6號
+ {   				// 為做出樂透號碼1~6號
   int tmp;
-  j=(rand()%49);//從範圍1~49數字中隨機取出一位數字
+  j=(rand()%49);		//從範圍1~49數字中隨機取出一位數字
   tmp=lotto1[i];
-  lotto1[i]=lotto1[j];//一維陣列
+  lotto1[i]=lotto1[j];		//一維陣列
   lotto1[j]=tmp;
  }
 
  printf("中獎號碼如下:\n");
- for (i=0;i<6;i++)//顯示1~6號
+ for (i=0;i<6;i++)		//顯示1~6號
   printf("%d ",lotto1[i]);
  printf("特別號: %d\n", lotto1[6]);
   printf("中獎號碼:");
- for(i=0;i<7;i++) //找出輸入值(i)
+ for(i=0;i<7;i++) 		//找出輸入值(i)
  {
-	for(j=0;j<7;j++)//隨機跑出的7位數(j)
+	for(j=0;j<7;j++)	//隨機跑出的7位數(j)
   {
    if(ptr->enter_num[i] == 0)
     break;
    else if(ptr->enter_num[i] == lotto1[j])
-   {//如輸入號碼與隨機號碼相符
+   {				//如輸入號碼與隨機號碼相符
     n++;
     printf("%d ",ptr->enter_num[i]); //用指標結構顯示出中獎號碼
    }
   }
  }
 
- if(n == 0) //n = 中獎號碼
+ if(n == 0) 			//n = 中獎號碼
  {
   printf("什麼都沒中");
  }
